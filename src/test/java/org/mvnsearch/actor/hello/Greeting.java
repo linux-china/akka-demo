367c6f3e500560d4a2e2f798dc8eac6bfcef3412
linux_china <libing.chen@gmail.com>
2019-01-07 15:16:09 -0800

    greeting

---
+++ src/test/java/org/mvnsearch/actor/hello/Greeting.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.actor.hello;
+
+/**
+ * greeting
+ *
+ * @author linux_china
+ */
+class Greeting {
+    public final String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+}
