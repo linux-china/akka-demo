73c10fed93b0d79a5493734e14b1267c97e7d310
linux_china <libing.chen@gmail.com>
2019-01-07 15:08:03 -0800

    Akka actor component annotation

---
+++ src/main/java/org/mvnsearch/akka/spring/ActorComponent.java
@@ -0,0 +1,22 @@
+package org.mvnsearch.akka.spring;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Akka actor component annotation
+ *
+ * @author linux_china
+ */
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActorComponent {
+}
