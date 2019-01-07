837d395df83cfd4b8bdad13db64a2cdfbd7404ba
linux_china <libing.chen@gmail.com>
2019-01-07 15:12:24 -0800

    akka with kotlin

---
+++ src/test/java/org/mvnsearch/actor/kotlin/HelloActorContext.kt
@@ -0,0 +1,25 @@
+package org.mvnsearch.actor.kotlin
+
+import akka.actor.AbstractLoggingActor
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.japi.pf.ReceiveBuilder
+
+/**
+ * hello actor
+ *
+ * @author linux_china
+ */
+class HelloKotlinActor : AbstractLoggingActor() {
+    override fun createReceive() = ReceiveBuilder().match(String::class.java) { log().info(it) }.build()
+}
+
+
+fun main() {
+    val actorSystem = ActorSystem.create("actorSystem1")
+    val helloActor = actorSystem.actorOf(Props.create(HelloKotlinActor::class.java))
+    helloActor.tell("jackie", ActorRef.noSender())
+    Thread.sleep(1000)
+}
+
