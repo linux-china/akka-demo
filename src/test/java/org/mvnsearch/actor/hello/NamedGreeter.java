f5781df90178ef6efbbd7a5cbe29dcfafd0796e7
linux_china <libing.chen@gmail.com>
2019-01-07 15:16:19 -0800

    Greeter with name

---
+++ src/test/java/org/mvnsearch/actor/hello/NamedGreeter.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.actor.hello;
+
+/**
+ * Greeter with name
+ *
+ * @author linux_china
+ */
+public class NamedGreeter {
+    public final String who;
+
+    public NamedGreeter(String who) {
+        this.who = who;
+    }
+}
