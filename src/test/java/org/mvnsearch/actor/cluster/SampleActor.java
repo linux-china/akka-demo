b7ba45fd28bfeb1be9e1947ea501370f73d5998e
linux_china <libing.chen@gmail.com>
2019-01-08 16:10:52 -0800

    sample actor

---
+++ src/test/java/org/mvnsearch/actor/cluster/SampleActor.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.actor.cluster;
+
+import akka.actor.AbstractActor;
+
+/**
+ * sample actor
+ *
+ * @author linux_china
+ */
+public class SampleActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(String.class, System.out::println)
+                .matchAny(obj -> {
+                    System.out.println("Unknown:" + obj.toString());
+                })
+                .build();
+    }
+}
