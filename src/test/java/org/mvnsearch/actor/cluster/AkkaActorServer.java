7ae2217256f7eaa940e3d9e832eb2088b549f242
linux_china <libing.chen@gmail.com>
2019-01-08 16:12:13 -0800

    Akka actor server

---
+++ src/test/java/org/mvnsearch/actor/cluster/AkkaActorServer.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.actor.cluster;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.cluster.client.ClusterClientReceptionist;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.mvnsearch.actor.remote.AkkaRemoteClient;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Akka actor server
+ *
+ * @author linux_china
+ */
+public class AkkaActorServer {
+
+    public static void main(String[] args) throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        Config config = ConfigFactory.parseURL(AkkaRemoteClient.class.getResource("/akka/cluster/cluster.conf"));
+        ActorSystem actorSystem = ActorSystem.create("ClusterSystem", config);
+        ActorRef ref = actorSystem.actorOf(Props.create(SampleActor.class), "sampleActor");
+        ClusterClientReceptionist.get(actorSystem).registerService(ref);
+        ref.tell("hello", ActorRef.noSender());
+        latch.await();
+    }
+}
